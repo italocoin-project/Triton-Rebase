6a6499122319abadf4b3f80c953deab2dc1b6aad
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-06-03 12:22:19 -0700

    Forgot the altchain

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1035,7 +1035,7 @@ difficulty_type Blockchain::get_next_difficulty_for_alternative_chain(const std:
   size_t target = get_ideal_hard_fork_version(bei.height) < 2 ? DIFFICULTY_TARGET_V1 : DIFFICULTY_TARGET_V2;
   size_t height = alt_chain.size();
   // calculate the difficulty target for the block and return it
-  return next_difficulty(height,timestamps, cumulative_difficulties, target);
+  return next_difficulty(timestamps, cumulative_difficulties, target,height);
 }
 //------------------------------------------------------------------
 // This function does a sanity check on basic things that all miner
